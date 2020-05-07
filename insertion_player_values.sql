3f9de0991fb50cef3f3b1befb2fb71fcd558e329
EmmadiDivyaSrujana <48437121+EmmadiDivyaSrujana@users.noreply.github.com>
2020-05-08 00:22:33 +0530

    Create insertion_player_values.sql

---
+++ insertion_player_values.sql
@@ -0,0 +1,15 @@
+insert into PLAYER values (
+    'PLR17410', 'IND1221', 2, 13, 58,7,24,800,74,120,20,1,'medium',3.2
+);
+insert into PLAYER values (
+    'PLR74203', 'AUS2174', 1, 6, 67, 10,39,463,41,210,140 ,1,'slow',8.5
+);
+insert into PLAYER values (
+    'PLR45987', 'SA5412', 3, 4, 99, 4, 47,985, 24,63,65,1,'medium-slow',11.2
+);
+insert into PLAYER values (
+    'PLR20147', 'NZ5687', 1, 12, 12, 1, 3,85, 52 ,10,74,1,'legspin',18.3
+);
+insert into PLAYER values (
+    'PLR65200', 'BAN9852', 2, 9, 4, 0,1 ,21,77, 30,2,1,'fast',17.3
+);
