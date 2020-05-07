efe8152cbee5ceb52faac5ca170f89059c7d203c
EmmadiDivyaSrujana <48437121+EmmadiDivyaSrujana@users.noreply.github.com>
2020-05-08 00:25:17 +0530

    Create insertion_plays_values.sql

---
+++ insertion_plays_values.sql
@@ -0,0 +1,15 @@
+insert into Plays values(
+    'IND1221','MAT101'
+);
+insert into Plays values(
+    'AUS2174','MAT201'
+);
+insert into Plays values(
+    'BAN9852','MAT301'
+);
+insert into Plays values(
+    'NZ5687','MAT401'
+);
+insert into Plays values(
+    'IND1221','MAT501'
+);
