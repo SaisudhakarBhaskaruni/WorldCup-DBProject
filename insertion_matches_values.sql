ee536d7098c93d4b43b349230295a7dcee9f2835
EmmadiDivyaSrujana <48437121+EmmadiDivyaSrujana@users.noreply.github.com>
2020-05-08 00:12:39 +0530

    Create insertion_matches_values.sql

---
+++ insertion_matches_values.sql
@@ -0,0 +1,15 @@
+insert into MATCHES values(
+    'MAT101',to_date('12-03-2011','dd-mm-yyyy'),to_timestamp('15:30','hh24:mi'),'India','Bangladesh','Bangladesh','India','Feroz Shah Kotla','UMP55200'
+);
+insert into MATCHES values(
+    'MAT201',to_date('15-03-2011','dd-mm-yyyy'),to_timestamp('9:30','hh24:mi'),'England','Australia','England','Australia','Eden Gardens','UMP41002'
+);
+insert into MATCHES values(
+    'MAT301',to_date('21-03-2011','dd-mm-yyyy'),to_timestamp('11:30','hh24:mi'),'Sri Lanka','Bangladesh','Bangladesh','Sri Lanka','M.A. Chidambaram','UMP74101'
+);
+insert into MATCHES values(
+    'MAT401',to_date('23-03-2011','dd-mm-yyyy'),to_timestamp('15:30','hh24:mi'),'New Zealand','South Africa','South Africa','New Zealand','Sardar Patel','UMP85201'
+);
+insert into MATCHES values(
+    'MAT501',to_date('26-03-2011','dd-mm-yyyy'),to_timestamp('8:30','hh24:mi'),'England','India','England','India','Wankhede','Ump52410'
+);
